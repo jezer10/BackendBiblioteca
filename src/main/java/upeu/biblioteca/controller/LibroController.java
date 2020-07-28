97ef4069b97f48bb1122815150e30f7d372f31ff
jezer10 <jezerrazuri@upeu.edu.pe>
2020-07-28 12:10:35 -0500

    first commit

--- src/main/java/upeu/biblioteca/controller/LibroController.java
+++ src/main/java/upeu/biblioteca/controller/LibroController.java
@@ -4,16 +4,19 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import upeu.biblioteca.service.LibroService;
-
+@CrossOrigin(origins="http://localhost:4200",maxAge=3600)
 @RestController
+@RequestMapping("/libro")
 public class LibroController {
 	@Autowired
 	private LibroService libroService;
-	@GetMapping("/libro")
+	@GetMapping("/all")
 	public List<Map<String, Object>> listar() {
 		// TODO Auto-generated method stub
 		return libroService.readAll();
